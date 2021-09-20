29e911f5a6c849adf4e82c83c08ea6b33e0ada0e
Konev_Daniil <konev.danKo@yandex.ru>
2021-09-20 19:49:24 +0300

    fix

--- Assets/Scripts/Game/Coin.cs
+++ Assets/Scripts/Game/Coin.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out Player player))
